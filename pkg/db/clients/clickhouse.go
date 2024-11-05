596f580ad829d7b584fd20e38bcba8fb6abee358
Nikita Rydanov <find.art.in.living@gmail.com>
2024-11-05 18:39:26 +0400

    chore: Formatting

--- pkg/db/clients/clickhouse.go
+++ pkg/db/clients/clickhouse.go
@@ -30,36 +30,52 @@ func NewClickHouseClient[S ri.StoredValue, P ri.StoredParams, Q ri.QueryBuilder[
 	var conn driver.Conn
 	log.S.Debug(
 		"Opening connection to the ClickHouse",
-		log.L().Tag(log.LogTagClickHouse),
+		log.L().
+			Tag(log.LogTagClickHouse),
 	)
-	conn, err = clickhouse.Open(&clickhouse.Options{
-		Addr: []string{fmt.Sprintf("%s:%s", host, port)},
-		Auth: clickhouse.Auth{
-			Database: database,
-			Username: username,
-			Password: password,
+	conn, err = clickhouse.Open(
+		&clickhouse.Options{
+			Addr: []string{
+				fmt.Sprintf(
+					"%s:%s",
+					host,
+					port,
+				),
+			},
+			Auth: clickhouse.Auth{
+				Database: database,
+				Username: username,
+				Password: password,
+			},
 		},
-	})
+	)
 	if err != nil {
 		log.S.Error(
 			"Failed to open a connection to the ClickHouse",
-			log.L().Tag(log.LogTagClickHouse).Error(err),
+			log.L().
+				Tag(log.LogTagClickHouse).
+				Error(err),
 		)
 		return nil, nil, err
 	}
 	log.S.Debug(
 		"Retrieving server version",
-		log.L().Tag(log.LogTagClickHouse),
+		log.L().
+			Tag(log.LogTagClickHouse),
 	)
 	version, err = conn.ServerVersion()
 	if err != nil {
 		log.S.Error(
 			"Failed to retrieve ClickHouse server version",
-			log.L().Tag(log.LogTagClickHouse).Error(err),
+			log.L().
+				Tag(log.LogTagClickHouse).
+				Error(err),
 		)
 		return nil, nil, err
 	}
-	return &ClickHouseClient[S, P, Q]{Connection: conn}, version, err
+	return &ClickHouseClient[S, P, Q]{
+		Connection: conn,
+	}, version, err
 }
 
 func (client *ClickHouseClient[S, P, Q]) AsyncInsertBatch(
@@ -69,30 +85,39 @@ func (client *ClickHouseClient[S, P, Q]) AsyncInsertBatch(
 ) error {
 	log.S.Debug(
 		"Inserting a batch to the database",
-		log.L().Tag(log.LogTagClickHouse),
+		log.L().
+			Tag(log.LogTagClickHouse),
 	)
 	var zeroInstance S
 	query := zeroInstance.GetInsertQuery()
 	log.S.Debug(
 		"Sending query to the database",
-		log.L().Tag(log.LogTagClickHouse).Add("query", query),
+		log.L().
+			Tag(log.LogTagClickHouse).
+			Add("query", query),
 	)
 	for i := 0; i < len(batch); i++ {
 		innerRepr := batch[i].AsArray()
 		err := client.Connection.AsyncInsert(
-			ctx, query, false, innerRepr...,
+			ctx,
+			query,
+			false,
+			innerRepr...,
 		)
 		if err != nil {
 			log.S.Error(
 				"Got an error while writing records to the database",
-				log.L().Tag(log.LogTagClickHouse).Error(err),
+				log.L().
+					Tag(log.LogTagClickHouse).
+					Error(err),
 			)
 			return err
 		}
 	}
 	log.S.Debug(
 		"Successfully saved batch to the database",
-		log.L().Tag(log.LogTagClickHouse),
+		log.L().
+			Tag(log.LogTagClickHouse),
 	)
 	return nil
 }
@@ -103,23 +128,30 @@ func (client *ClickHouseClient[S, P, Q]) SelectNextBatch(
 ) (result []P, err error) {
 	log.S.Debug(
 		"Retrieving a new batch from the database",
-		log.L().Tag(log.LogTagClickHouse),
+		log.L().
+			Tag(log.LogTagClickHouse),
 	)
 	query := queryBuilder.GetSelectQuery()
 	log.S.Debug(
 		"Sending query to the database",
-		log.L().Tag(log.LogTagClickHouse).Add("query", query),
+		log.L().
+			Tag(log.LogTagClickHouse).
+			Add("query", query),
 	)
 	if err = client.Connection.Select(ctx, &result, query); err != nil {
 		log.S.Error(
 			"Got an error while retrieving records from the database",
-			log.L().Tag(log.LogTagClickHouse).Error(err),
+			log.L().
+				Tag(log.LogTagClickHouse).
+				Error(err),
 		)
 		return nil, err
 	}
 	log.S.Debug(
 		"Successfully got records from the database",
-		log.L().Tag(log.LogTagClickHouse).Add("count", len(result)),
+		log.L().
+			Tag(log.LogTagClickHouse).
+			Add("count", len(result)),
 	)
 	return result, nil
 }
