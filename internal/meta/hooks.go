f6d53307894c5cc2cf786543b991aad111f86505
Nikita Rydanov <find.art.in.living@gmail.com>
2025-01-29 17:05:50 +0400

    refactor: Removed default hooks logic. Why is it still here?

--- internal/meta/hooks.go
+++ internal/meta/hooks.go
@@ -3,9 +3,7 @@ package meta
 import (
 	"context"
 
-	"orb/runner/pkg/log"
 	"orb/runner/pkg/runner/hooks"
-	"orb/runner/pkg/util"
 )
 
 type VerifyApiHooks struct {
@@ -18,30 +16,4 @@ func (srv *VerifyApiHooks) AfterBatch(
 	ctx context.Context,
 	results []VerifyResult,
 ) {
-	select {
-	case <-ctx.Done():
-		return
-	default:
-		successesWithScores := util.Reduce(
-			util.Map(
-				results,
-				func(res VerifyResult) bool {
-					return res.GetStatusCode() == 200 &&
-						res.MetaResponse.Score != nil
-				},
-			),
-			0,
-			func(acc int, v bool) int {
-				if v {
-					return acc + 1
-				}
-				return acc
-			},
-		)
-		log.S.Info(
-			"Post-analyzed the processed batch",
-			log.L().
-				Add("successes_with_scores", successesWithScores),
-		)
-	}
 }
