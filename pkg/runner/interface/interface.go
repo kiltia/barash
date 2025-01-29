2f35e59a72c7f98f0517b341323b83794697770d
Nikita Rydanov <find.art.in.living@gmail.com>
2025-01-29 17:02:18 +0400

    refactor: Simplified StoredValue interface

--- pkg/runner/interface/interface.go
+++ pkg/runner/interface/interface.go
@@ -8,28 +8,9 @@ import (
 
 type (
 	StoredValue interface {
-		// Return **parameterized** INSERT query for inserting a
-		// row into the database.
-		//
-		// Note that, by design, the receiver value shouldn't
-		// matter and it is possible for it to be zero-value. So don't
-		// rely on it, or check before using it in your implementation.
-		//
-		// Ideally, you should only return a single parameterized
-		// (templated) query.
-		GetInsertQuery() string
-
 		// Return CREATE TABLE query for creating a new table
 		// that will be used to store every run's data in a row.
 		GetCreateQuery() string
-
-		// Return stored values as an array.
-		//
-		// The values are used as parameters to the INSERT query.
-		AsDict() map[string]any
-
-		// Get value of the status code field.
-		GetStatusCode() int
 	}
 
 	StoredParams interface {
