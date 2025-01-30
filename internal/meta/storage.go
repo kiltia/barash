02b45e0312348be6c7fd7ae4ff181fbd58f6e624
Nikita Rydanov <find.art.in.living@gmail.com>
2025-01-30 13:29:07 +0400

    fix: Fixed some VerifyResult types so conversion fails in std code

--- internal/meta/storage.go
+++ internal/meta/storage.go
@@ -8,45 +8,38 @@ import (
 )
 
 type VerifyResult struct {
-	Duns                   string    `ch:"duns"`
-	IsActive               bool      `ch:"is_active"`
-	Url                    string    `ch:"url"`
-	VerificationUrl        string    `ch:"verification_url"`
-	StatusCode             uint16    `ch:"status_code"`
-	Error                  string    `ch:"error"`
-	ErrorCode              string    `ch:"error_code"`
-	ErrorType              string    `ch:"error_type"`
-	ErrorRepr              string    `ch:"error_repr"`
-	AttemptsNumber         uint8     `ch:"attempts_number"`
-	CrawlerErrors          []string  `ch:"crawler_errors"`
-	CrawlFails             []string  `ch:"crawl_fails"`
-	CrawledPages           []string  `ch:"crawled_pages"`
-	NumErrors              int       `ch:"num_errors"`
-	NumFails               int       `ch:"num_fails"`
-	NumSuccesses           int       `ch:"num_successes"`
-	Features               string    `ch:"features"`
-	MatchMaskDetails       string    `ch:"match_mask_details"`
-	MmName                 string    `ch:"mm_name"`
-	MmAddress1             string    `ch:"mm_address1"`
-	MmAddress2             string    `ch:"mm_address2"`
-	MmCity                 string    `ch:"mm_city"`
-	MmState                string    `ch:"mm_state"`
-	MmCountry              string    `ch:"mm_country"`
-	MmDomainNameSimilarity float64   `ch:"mm_domain_name_similarity"`
-	FinalUrl               string    `ch:"final_url"`
-	Score                  float64   `ch:"score"`
-	Tag                    string    `ch:"tag"`
+	Duns                   string             `ch:"duns"`
+	IsActive               bool               `ch:"is_active"`
+	Url                    string             `ch:"url"`
+	VerificationUrl        string             `ch:"verification_url"`
+	StatusCode             int32              `ch:"status_code"`
+	Error                  string             `ch:"error"`
+	ErrorCode              string             `ch:"error_code"`
+	ErrorType              string             `ch:"error_type"`
+	ErrorRepr              string             `ch:"error_repr"`
+	AttemptsNumber         int32              `ch:"attempts_number"`
+	CrawlerErrors          []string           `ch:"crawler_errors"`
+	CrawlFails             []string           `ch:"crawl_fails"`
+	CrawledPages           []string           `ch:"crawled_pages"`
+	NumErrors              int32              `ch:"num_errors"`
+	NumFails               int32              `ch:"num_fails"`
+	NumSuccesses           int32              `ch:"num_successes"`
+	Features               string             `ch:"features"`
+	MatchMaskDetails       string             `ch:"match_mask_details"`
+	MmName                 string             `ch:"mm_name"`
+	MmAddress1             string             `ch:"mm_address1"`
+	MmAddress2             string             `ch:"mm_address2"`
+	MmCity                 string             `ch:"mm_city"`
+	MmState                string             `ch:"mm_state"`
+	MmCountry              string             `ch:"mm_country"`
+	MmDomainNameSimilarity float64            `ch:"mm_domain_name_similarity"`
+	FinalUrl               string             `ch:"final_url"`
+	Score                  float64            `ch:"score"`
+	Tag                    string             `ch:"tag"`
 	ResponseTimes          map[string]float32 `ch:"response_times"`
 	ResponseCodes          map[string]uint16  `ch:"response_codes"`
-	FeStatusCode           []uint16    `ch:"metrics.fe_status_code"`
-	FeResponseTime         []float32   `ch:"metrics.fe_response_time"`
-	FtStatusCode           []uint16    `ch:"metrics.ft_status_code"`
-	FtResponseTime         []float32   `ch:"metrics.ft_response_time"`
-	ScorerStatusCode       []uint16    `ch:"metrics.scorer_status_code"`
-	ScorerResponseTime     []float32   `ch:"metrics.scorer_response_time"`
-	Timestamp              time.Time `ch:"timestamp"`
-	Ts                     time.Time `ch:"ts"`
-	CorrTs                 time.Time `ch:"corr_ts"`
+	Timestamp              time.Time          `ch:"ts"`
+	CorrTs                 time.Time          `ch:"corr_ts"`
 }
 
 // Implement the [rinterface.StoredValue] interface.
