89a2ad2d7fc75bbcaab168ba2e1680cd26ca4108
Nikita Rydanov <find.art.in.living@gmail.com>
2025-08-13 13:59:58 +0400

    refactor: Added comments for config code

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	var cfg config.Config
+	// load enviroment variables + .env file, then base config file
+	config.Init()
+	// save variables from .env + base config file
 	config.Load(&cfg)
 
 	internal.RunApplication(&cfg)
